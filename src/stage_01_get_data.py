ecf807bf10c3abff457acc94a5c73b5776d4a20e
Karthik <mailmekarthik001@gmail.com>
2023-02-17 14:57:29 +0530

    Stage 01 is added

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -26,29 +26,44 @@ logging.basicConfig(
     )
 
 
-def main(config_path, params_path):
+def main(config_path):
     ## read config files
     config = read_yaml(config_path)
-    params = read_yaml(params_path)
 
-    train = datasets.MNIST("../data", train=True, download=True, transform=transform)
-    test = datasets.MNIST("../data", train=False, download=True, transform=transform)
+    device_config = {"DEVICE" : 'cuda' if torch.cuda.is_available() else 'cpu'}
+    config.update(device_config)
+    logging.info("Device is : " + config["DEVICE"])
+
+
+    if config["DEVICE"] == "cuda":
+        cuda_kwargs = {"num_workers": 1, "pin_memory": True, "shuffle": True}
+        train_kwargs.update(cuda_kwargs)
+        test_kwargs.update(cuda_kwargs)
+
+    train_kwargs = {"batch_size": config["params"]["BATCH_SIZE"]}
+    test_kwargs = {"batch_size": config["params"]["TEST_BATCH_SIZE"]}
+    transform = transforms.Compose([transforms.ToTensor()])
+    
+    train = datasets.MNIST(config["source_data_dirs"]["data"], train=True, download=True, transform=transform)
+    test = datasets.MNIST(config["source_data_dirs"]["data"], train=False, download=True, transform=transform)
+    
+
 
     train_loader = torch.utils.data.DataLoader(train, **train_kwargs)
     test_loader = torch.utils.data.DataLoader(test, **test_kwargs)
-    pass
+    logging.info("Added Data in the Folder : " + config["source_data_dirs"]["data"])
+
 
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
-    args.add_argument("--params", "-p", default="params.yaml")
     parsed_args = args.parse_args()
 
     try:
         logging.info("\n********************")
         logging.info(f">>>>> stage {STAGE} started <<<<<")
-        main(config_path=parsed_args.config, params_path=parsed_args.params)
+        main(config_path=parsed_args.config)
         logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
     except Exception as e:
         logging.exception(e)
